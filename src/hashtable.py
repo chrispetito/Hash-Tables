860ddc4b10595f15dcedb8bacf7c52c6edb7ebc2
chrispetito <christopherpetito@gmail.com>
2019-10-01 15:41:37 -0700

    fill out retrieve, resize, insert methods; most tests passing

--- src/hashtable.py
+++ src/hashtable.py
@@ -51,7 +51,17 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+
+        index = self._hash_mod(key)
+        if self.storage[index] is not None:
+            current = self.storage[index]
+            while current.next is not None:
+                current = current.next
+            current.next = LinkedPair(key, value)
+            return
+        else:
+            self.storage[index] = LinkedPair(key, value)
+        
 
 
 
@@ -63,7 +73,12 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        index = self._hash_mod(key)
+        if self.storage[index] is None:
+            print('Warning: key not found')
+        else:
+            self.storage[index] = None
+        
 
 
     def retrieve(self, key):
@@ -74,8 +89,22 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
-
+        index = self._hash_mod(key)
+        print(f'HERE IS THE KEY: {key}')
+        # print(f'************** {self.storage[index]}')
+        if self.storage[index] is None:
+            return None
+        else:
+            current = self.storage[index]
+            if current.key == key:
+                return current.value
+            while current is not None:
+                if current.key == key:
+                    return current.value
+                    # print(current.value)
+                else:
+                    current = current.next
+            return None
 
     def resize(self):
         '''
@@ -84,7 +113,16 @@ class HashTable:
 
         Fill this in.
         '''
-        pass
+        self.capacity *= 2
+        store = [None] * self.capacity
+        for i in [item for item in self.storage if item != None]:
+            current = i
+            while current is not None:
+                store[self._hash_mod(current.key)] = LinkedPair(
+                    current.key, current.value)
+                current = current.next
+
+        self.storage = store
 
 
 
